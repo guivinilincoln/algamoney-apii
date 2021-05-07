f86f391fa118031bb11052fe9f225b29d716398a
Guilherme Lincoln <gui.vini.lincoln@gmail.com>
2021-05-06 21:48:53 -0300

    Commit inicial

---
+++ src/main/java/com/example/algamoneyapi/AlgamoneyApiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.algamoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyApiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyApiiApplication.class, args);
+	}
+
+}
